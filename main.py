c87d8bf9975f715cb8ba051f5acf10195979cc35
carolinux <karolina.alexiou@teralytics.ch>
2016-01-23 14:18:23 +0100

    Can now alternate colors of grid

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import sys
 import skimage.io as io
 
 
-Line = namedtuple("Line","sx sy ex ey width")
-Rect = namedtuple("Rect","bottomx bottomy topx topy")
+Line = namedtuple("Line","sx sy ex ey width color")
+Rect = namedtuple("Rect","bottomx bottomy topx topy hierarchy")
 
 
-def draw_line_on_picture(pic, line, color=[255,0,0]):
+def draw_line_on_picture(pic, line):
     if line.sx == line.ex:
-        pic[line.sy:line.ey,line.sx: line.sx+line.width] = color
+        pic[line.sy:line.ey,line.sx: line.sx+line.width] = line.color
     elif line.sy == line.ey:
-        pic[line.sy:line.sy+line.width,line.sx: line.ex] = color
+        pic[line.sy:line.sy+line.width,line.sx: line.ex] = line.color
     else:
         raise Exception("Non straight lines not supported") # fuck homophobia tho
     return pic
@@ -21,6 +21,14 @@ def draw_line_on_picture(pic, line, color=[255,0,0]):
 parts = []
 main_pic = None 
 lw = 3
+
+colors=[[255,0,0],[0,255,0],[0,0,255],[255,255,255]]
+
+def get_color(idx):
+
+    return colors[idx % len(colors)]
+
+
 def divide(x,y):
     global parts
     global main_pic
@@ -29,19 +37,20 @@ def divide(x,y):
     for i,rect in enumerate(parts):
         if  x>= rect.bottomx and x<rect.topx and y>=rect.bottomy and y<rect.topy:
             #import ipdb; ipdb.set_trace()
-            # TODO remove this from parts array and replace with 4 more
             mx = rect.bottomx + (rect.topx - rect.bottomx)/2
             my = rect.bottomy + (rect.topy - rect.bottomy)/2
-            line1 = Line(mx,rect.bottomy,mx,rect.topy, lw)
-            line2 = Line(rect.bottomx,my,rect.topx,my, lw)
+            new_hierarchy = rect.hierarchy + 1
+            color = get_color(new_hierarchy)
+            line1 = Line(mx,rect.bottomy,mx,rect.topy, lw,color)
+            line2 = Line(rect.bottomx,my,rect.topx,my, lw,color)
             print line1, line2
             main_pic = draw_line_on_picture(main_pic, line1)
             main_pic = draw_line_on_picture(main_pic, line2)
             parts.remove(rect)
-            parts.append(Rect(rect.bottomx, rect.bottomy, mx,my))
-            parts.append(Rect(mx,my,rect.topx, rect.topy))
-            parts.append(Rect(rect.bottomx,my,mx, rect.topy))
-            parts.append(Rect(mx,rect.bottomy,rect.topx, my))
+            parts.append(Rect(rect.bottomx, rect.bottomy, mx,my,new_hierarchy))
+            parts.append(Rect(mx,my,rect.topx, rect.topy,new_hierarchy))
+            parts.append(Rect(rect.bottomx,my,mx, rect.topy, new_hierarchy))
+            parts.append(Rect(mx,rect.bottomy,rect.topx, my, new_hierarchy))
             return
 
 
@@ -63,12 +72,13 @@ def plot():
 def main(args):
     global parts
     global main_pic
-    imagePath = "/home/carolinux/Pictures/artwerk.jpg"
+    imagePath = args[0] #"/home/carolinux/Pictures/artwerk.jpg"
     main_pic = io.imread(imagePath)
-    parts = [Rect(0,0,main_pic.shape[1], main_pic.shape[0])]
+    print("Size {}".format(main_pic.shape))
+    parts = [Rect(0,0,main_pic.shape[1], main_pic.shape[0],0)]
     divide(1,1)
     plot()
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
